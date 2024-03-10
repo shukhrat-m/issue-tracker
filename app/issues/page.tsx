30c0fd099ae03a46de09ae76118eb6b0d66390ec
Shukhrat M. <mukhammadiev.sh@gmail.com>
2024-03-10 11:42:06 +0100

    Add SimpleMDE editor

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@radix-ui/themes'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
 
 const IssuesPage = () => {
   return (
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./nav-bar";
 import '@radix-ui/themes/styles.css';
-import './theme-config.css'
-import { Theme, ThemePanel } from '@radix-ui/themes';
+import './theme-config.css';
+import { Theme } from '@radix-ui/themes';
 
 const inter = Inter({ 
   subsets: ["latin"],
